d1d899ba1361b425111fd1a97e61462085a1fff9
austiniqer <63011075@kmitl.ac.th>
2022-11-24 18:16:19 +0700

    add(some): making some progress

---
+++ DesignPatterPractise/builder/src/components/GPSNavigator.java
@@ -0,0 +1,16 @@
+package components;
+
+public class GPSNavigator {
+    private String route;
+
+    public GPSNavigator(){
+        this.route = "221b, Baker Street, London ot Scotland Yard, 8-10 Broadway, London";
+    }
+    public GPSNavigator(String manualRoute) {
+        this.route = manualRoute;
+    }
+
+    public String getRoute(){
+        return route;
+    }
+}
